f6dad78cb104c4022cbbf7d186ca2ff4414d911a
perrishnikov <perrishnikov@gmail.com>
2019-08-16 09:44:04 -0500

    added DFE

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const validate = {
     return str1.length > min;
   },
   isValidUOM: (value) => {
-    const stuff = ['mg', 'mcg', 'g', 'ml', 'l', 'oz', 'CFU', ''];
+    const stuff = ['mg', 'mcg', 'g', 'ml', 'l', 'oz', 'CFU', 'mcg DFE', 'mg DFE', ''];
     console.assert(stuff.includes(value));
     return stuff.includes(value);
   }
